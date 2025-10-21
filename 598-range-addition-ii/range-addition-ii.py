207264e70739b672c6848586b14352edbe4af663
ritikasolankii <95336707+ritika-hackerone@users.noreply.github.com>
2025-10-21 23:28:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (39.76%) - LeetSync

---
+++ 598-range-addition-ii/range-addition-ii.py
@@ -0,0 +1,4 @@
+class Solution:
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+        return min(ops, key=lambda e: e[0])[0] * min(ops, key=lambda e: e[1])[1] if ops else m * n
+        
