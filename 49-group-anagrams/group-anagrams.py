cafe28e81895e8656caca928e53daf6146d3e5ca
ritikasolankii <95336707+ritika-hackerone@users.noreply.github.com>
2026-09-16 13:43:29 +0530

    Time: 19 ms (19.73%) | Memory: 23.9 MB (17.54%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,9 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        createMap = defaultdict(list)
-        for n in strs:
-            sortedS = ''.join(sorted(n))
-            createMap[sortedS].append(n)
-        return list(createMap.values())
+        res = defaultdict(list)
+        for s in strs:
+            count = [0]*26
+            for c in s:
+                count[ord(c)-ord('a')] += 1
+            res[tuple(count)].append(s)
+
+        return list(res.values())
 
         
